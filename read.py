a4802a4becebeee26d70ad4c956eeaeeec0c88e5
Scott.yang 楊慶堂 <scott.yang@cohesiondata.com>
2023-12-15 10:49:11 +0800

    調整輸出格式，供 influxdb 使用

--- read.py
+++ read.py
@@ -1,21 +1,26 @@
-from datetime import datetime
 from bleak import BleakClient
 import asyncio
 import struct
 import logging
 
-async def read_characteristic_value(address, service_uuid, characteristic_uuid):
-    async with BleakClient(address, timeout=60) as client:
+async def read_characteristic_value(address, characteristic_uuid):
+    async with BleakClient(address, timeout=120) as client:
+        # 遍歷每個服務並獲取其特徵
+        # for service in client.services:
+        #     print(f"Service: {service.uuid}")
+        #     for char in service.characteristics:
+        #         print(f"Characteristic: {char.uuid}")
+
+        MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+
         value = await client.read_gatt_char(characteristic_uuid)
         humidity = value[2]
         voltage = struct.unpack('H', value[3:5])[0]/ 1000
         batteryLevel = round((voltage - 2.1),2) * 100
         temperature = struct.unpack('H', value[:2])[0] / 100
 
-        print(f"Tempterature: {temperature}°C")
-        print(f"    Humidity: {humidity}%")
-        print(f"batteryLevel: {batteryLevel}%")
-        print(f"        Time: {datetime.now().strftime('%H:%M:%S')}")
+        print(f"溫溼度,設備={''.join(map(chr, model_number))} Tempterature={temperature},Humidity={humidity}i,batteryLevel={batteryLevel}")
 
 log_level = logging.INFO
 logging.basicConfig(
@@ -24,10 +29,9 @@ logging.basicConfig(
 )
 
 address = "A99E3E25-FE1B-1B67-F090-613DF306A501"  # Replace with the actual address of the Bluetooth device
-service_uuid = "0000fe95-0000-1000-8000-00805f9b34fb"  # Replace with the UUID of the service
 characteristic_uuid = "EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6"  # Replace with the UUID of the characteristic
 
-asyncio.run(read_characteristic_value(address, service_uuid, characteristic_uuid))
+asyncio.run(read_characteristic_value(address, characteristic_uuid))
 
 # A4:C1:38:EF:5D:B6: MJWSD05MMC A99E3E25-FE1B-1B67-F090-613DF306A501
 # AdvertisementData(local_name='MJWSD05MMC', service_data={'0000fe95-0000-1000-8000-00805f9b34fb': b'\x10Y2(!\xb6]\xef8\xc1\xa4'}, rssi=-80)
